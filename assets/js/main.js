7c1da5dd5d9f3c35af0e32ba68c4da9b4bcf45c5
onaseer07 <onaseer07@gmail.com>
2019-04-13 14:45:23 -0500

    testing layout - pushed up questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
-//when user clicks on the next button it will fade out the id 0 & fade in id 1
-    //setting id 0 css visibility to hidden, display none
+//when user clicks on next button the scene fades out the current question and fades in a new one
 
+// Array created to hold questions
     const questions = ['business-information','business-expenses']
+    //storing questions in variables
     let $busInfo = $(`#${questions[0]}`);
     let $busExp = $(`#${questions[1]}`);
 
     $('#next').on('click',function(){
-
+        //next tag click will fade out the current question
         $busInfo.fadeOut()
                 .css({'display':'none'});
-                
+         //and fade in the next question       
         $busExp.fadeIn()
                .css({'visibility':'visible'});
     })
 
     $('#back').on('click', function() {
-        
+        //back button will fade in the previous question
         $busInfo.fadeIn()
                 .css({'display':'block'});
-
+        // and fade out the current question 
         $busExp.fadeOut()
                 .css(
                     {
